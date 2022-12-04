3b07875b209c3b8de528e941bbace30cd4d2216a
aarzilli <alessandro.arzilli@gmail.com>
2022-12-04 06:28:14 +0100

    day 04

---
+++ 04.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	. "aoc/util"
+	"fmt"
+	"os"
+)
+
+func pf(fmtstr string, any ...interface{}) {
+	fmt.Printf(fmtstr, any...)
+}
+
+func pln(any ...interface{}) {
+	fmt.Println(any...)
+}
+
+func contains(a, b []int) bool {
+	return a[0] <= b[0] && a[1] >= b[1]
+}
+
+func overlaps(a, b []int) bool {
+	if contains(a, b) || contains(b, a) {
+		return true
+	}
+	return contains1(a, b[0]) || contains1(a, b[1])
+}
+
+func contains1(a []int, n int) bool {
+	return n >= a[0] && n <= a[1]
+}
+
+func main() {
+	lines := Input(os.Args[1], "\n", true)
+	pf("len %d\n", len(lines))
+	cnt := 0
+	cnt2 := 0
+	for _, line := range lines {
+		v := Getints(line, false)
+		if contains(v[:2], v[2:]) || contains(v[2:], v[:2]) {
+			cnt++
+		}
+		if overlaps(v[:2], v[2:]) || overlaps(v[2:], v[:2]) {
+			pf("overlap %s\n", line)
+			cnt2++
+		}
+	}
+	Sol(cnt)
+	Sol(cnt2)
+}
